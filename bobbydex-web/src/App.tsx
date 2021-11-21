c6f19dcfa677c75c0581ae594ec0965fbc25b412
Robert Mruczek <rtmruczek@gmail.com>
2021-11-21 16:03:58 -0500

    chore: lint

--- bobbydex-web/src/App.tsx
+++ bobbydex-web/src/App.tsx
@@ -5,6 +5,7 @@ import authMachine from './machines/auth.machine';
 
 function App() {
   const [_state, send] = useMachine(authMachine, { devTools: true });
+  console.log(_state);
   send({ type: 'TRY_AUTH' });
   return <div>{process.env.NODE_ENV}</div>;
 }
